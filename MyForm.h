7bfe40e3f050949cce8249ad7c24e1242510421c
vasya <e.semenuta@yandex.ru>
2019-11-27 16:02:20 +0300

    just buttons

---
+++ MyForm.h
@@ -0,0 +1,153 @@
+#pragma once
+//#include <string>
+#include <msclr/marshal_cppstd.h>
+#include "..\mp2-lab3-calculator\mp2-lab3-calculator\TCalculator.h"
+
+namespace CppWinForm1 {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Summary for MyForm
+	/// </summary>
+	public ref class MyForm : public System::Windows::Forms::Form
+	{
+	public:
+		MyForm(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: Add the constructor code here
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		~MyForm()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+	private: System::Windows::Forms::Button^  button1;
+	protected:
+	private: System::Windows::Forms::TextBox^  textBox1;
+	private: System::Windows::Forms::TextBox^  textBox2;
+	private: System::Windows::Forms::Label^  label1;
+	private: System::Windows::Forms::Label^  label2;
+
+
+
+	protected:
+
+	private:
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->SuspendLayout();
+			// 
+			// button1
+			// 
+			this->button1->Location = System::Drawing::Point(312, 12);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(81, 23);
+			this->button1->TabIndex = 0;
+			this->button1->Text = L"Обновить";
+			this->button1->UseVisualStyleBackColor = true;
+			// 
+			// textBox1
+			// 
+			this->textBox1->Location = System::Drawing::Point(312, 47);
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->Size = System::Drawing::Size(40, 20);
+			this->textBox1->TabIndex = 1;
+			// 
+			// textBox2
+			// 
+			this->textBox2->Location = System::Drawing::Point(312, 73);
+			this->textBox2->Name = L"textBox2";
+			this->textBox2->Size = System::Drawing::Size(39, 20);
+			this->textBox2->TabIndex = 2;
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Location = System::Drawing::Point(358, 50);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(135, 13);
+			this->label1->TabIndex = 3;
+			this->label1->Text = L"Вероятность добавления";
+			this->label1->Click += gcnew System::EventHandler(this, &MyForm::label1_Click_1);
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->Location = System::Drawing::Point(358, 76);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(136, 13);
+			this->label2->TabIndex = 4;
+			this->label2->Text = L"Вероятность исключения";
+			// 
+			// MyForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(545, 261);
+			this->Controls->Add(this->label2);
+			this->Controls->Add(this->label1);
+			this->Controls->Add(this->textBox2);
+			this->Controls->Add(this->textBox1);
+			this->Controls->Add(this->button1);
+			this->Name = L"MyForm";
+			this->Text = L"MyForm";
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
+		}
+#pragma endregion
+	private: System::Void button1_Click(System::Object^  sender, System::EventArgs^  e) {
+		try {
+			std::string inf;
+			inf = msclr::interop::marshal_as<std::string>(textBox1->Text);
+			TCalculator calc;
+			calc.SetExpr(inf);
+			if (!calc.CheckBrackets()) throw - 1;
+			calc.ToPostfix();
+			double res = calc.Calc();
+			label1->Text = Convert::ToString(res);
+		}
+		catch (int ex) {
+			label1->Text = "oof error";
+		}
+		//label1->Text = gcnew System::String(inf.c_str());
+	}
+	private: System::Void label1_Click(System::Object^  sender, System::EventArgs^  e) {
+	}
+	private: System::Void textBox1_TextChanged(System::Object^  sender, System::EventArgs^  e) {
+	}
+	private: System::Void label1_Click_1(System::Object^  sender, System::EventArgs^  e) {
+	}
+};
+}
